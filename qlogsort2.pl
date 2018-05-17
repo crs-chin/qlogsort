7c368fa48f0e77d245768e1f54a2e2861ea4198a
Cross Chin <chin@qdeb>
2018-05-17 14:48:41 +0800

    Update version to 1.0.5

--- qlogsort2.pl
+++ qlogsort2.pl
@@ -172,7 +172,7 @@ decimal digits
 
 =cut
 
-my $VERSION = 'version 1.0.2 (c) crs.chin@gmain.com';
+my $VERSION = 'version 1.0.5 (c) crs.chin@gmain.com';
 
 # configured tag handlers table
 my %TAG_HANDLER_CONFIG_TABLE = (
